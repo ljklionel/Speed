33b670defff0d07e2c393ff6fa6ea61789fa8011
ljklionel <lionlim97@hotmail.com>
2022-11-03 11:53:11 +0800

    fix database migration

--- Server/Models/ApplicationUser.cs
+++ Server/Models/ApplicationUser.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Speed.Shared;
 
 namespace Speed.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public List<Score> Scores { get; set; } = new List<Score>();
     }
 }
